670e8890f8f2ec2eb94e088f075e9756487629f2
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-10-16 12:08:02 -0300

    Criando função de adicionar no vetor

---
+++ src/com/herbert/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,10 @@
+package com.herbert.estruturadados.vetor.teste;
+
+import com.herbert.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+        int i=0;
+    }
+}
